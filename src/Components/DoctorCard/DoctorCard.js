e5b71bfe9314cfb06c0c7a274db82b24a4f0f13d
mc-011g <mcarmichael011@gmail.com>
2024-05-07 11:51:46 -0400

    Updated DoctorCard to allow multiple appointments

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import doctorImage from "../FindDoctorSearch/images/icons8-doctor-100.png";
 import "./DoctorCard.css";
 import Popup from 'reactjs-popup';
@@ -9,7 +9,6 @@ import xIcon from "../AppointmentForm/images/icons8-x-48.png";
 const DoctorCard = ({ name, speciality, experience, ratings }) => {
     const [showModal, setShowModal] = useState(false);
     const [appointments, setAppointments] = useState([]);
-    const prevAppoinment = useRef(appointments);
     const [cancelAppointment, setCancelAppointment] = useState(false);
 
     const handleCancel = (appointmentId) => {
@@ -24,6 +23,7 @@ const DoctorCard = ({ name, speciality, experience, ratings }) => {
             ...appointmentData,
         };
         const updatedAppointments = [...appointments, newAppointment];
+
         setAppointments(updatedAppointments);
         setShowModal(false);
     };
@@ -36,20 +36,21 @@ const DoctorCard = ({ name, speciality, experience, ratings }) => {
     }, [])
 
     useEffect(() => {
-        if (((cancelAppointment == true) && (localStorage.getItem("appointmentData")))) {
+        if (((cancelAppointment === true) && (localStorage.getItem("appointmentData")))) {
             localStorage.removeItem("appointmentData");
             window.dispatchEvent(new Event("storage"));
         }
-        if ((appointments.length > 0) && (!(JSON.parse(localStorage.getItem("appointmentData"))))) {
+        if ((appointments.length > 0)) {
             localStorage.setItem("appointmentData", JSON.stringify(appointments));
             window.dispatchEvent(new Event("storage"));
         }
-    }, [appointments]);
+
+    }, [appointments, cancelAppointment]);
 
     return (
         <div className="doctor-card">
             <div className="doctor-card-info">
-                <img className="doctor-card-image" src={doctorImage} />
+                <img className="doctor-card-image" src={doctorImage} alt="Doctor" />
                 <div style={{ fontSize: "18px" }}><b>{name}</b></div>
                 <div>{speciality}</div>
                 <div>{experience} years of experience</div>
@@ -57,8 +58,8 @@ const DoctorCard = ({ name, speciality, experience, ratings }) => {
             </div>
 
             <Popup trigger={
-                <button className={`book-appointment-button ${((appointments.length > 0) && (name == appointments[0].doctorName)) ? 'cancel-appointment-button' : ''}`}>
-                    {((appointments.length > 0) && (name == appointments[0].doctorName)) ? (
+                <button className={`book-appointment-button ${((appointments.length > 0) && (appointments.find((appointment) => appointment.doctorName === name))) ? 'cancel-appointment-button' : ''}`}>
+                    {(appointments.find((appointment) => appointment.doctorName === name)) ? (
                         <div>Cancel appointment</div>
                     ) : (
                         <div>Book appointment</div>
@@ -71,17 +72,25 @@ const DoctorCard = ({ name, speciality, experience, ratings }) => {
             >
                 {(close) => (
                     <>
-                        {(appointments.length > 0) ? (
+                        {((appointments.find((appointment) => appointment.doctorName === name))) ? (
                             <div className="appointment-booked">
-                                <img src={xIcon} className="closeButton" onClick={close} />
+                                <img src={xIcon} className="closeButton" onClick={close} alt="" />
                                 <h3>Appointment Booked</h3>
+
                                 {appointments.map((appointment) => (
-                                    <div className="bookedInfo" key={appointment.id}>
-                                        <p>Name: {appointment.name}</p>
-                                        <p>Phone Number: {appointment.phoneNumber}</p>
-                                        <button className="book-appointment-button cancel-appointment-button" onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
-                                    </div>
+                                    <>
+                                        {(appointment.doctorName === name) ? (
+                                            <div className="bookedInfo" key={appointment.id}>
+                                                <p>Name: {appointment.name}</p>
+                                                <p>Phone Number: {appointment.phoneNumber}</p>
+                                                <button className="book-appointment-button cancel-appointment-button" onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
+                                            </div>
+                                        ) : (
+                                            <></>
+                                        )}
+                                    </>
                                 ))}
+
                             </div>
                         ) : (
                             <AppointmentForm doctorName={name} doctorSpecialty={speciality} doctorExperience={experience} doctorRatings={ratings} doctorImage={doctorImage} onSubmit={handleFormSubmit} closeModal={close} xIcon={xIcon} />
